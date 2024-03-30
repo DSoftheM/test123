c5b4357b676841f20a1ae924e90f2326f87cb09a
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-03-30 20:36:53 +0500

    wef

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import photoPath from "./photo.avif";
+
+function App() {
+    return (
+        <>
+            <img src="src/photo.avif" alt="" />
+            123
+            <img src={photoPath} alt="" />
+        </>
+    );
+}
+
+export default App;
+
