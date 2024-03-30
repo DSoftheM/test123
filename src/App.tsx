c5af1d95546c36930ae16492954517c076ad6ca8
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-03-30 20:39:33 +0500

    wef

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { Route, Routes } from "react-router-dom";
 import photoPath from "./photo.avif";
 
-function App() {
+function Home() {
     return (
         <>
             <img src="src/photo.avif" alt="" />
-            123
+            Home
+            <img src={photoPath} alt="" />
+        </>
+    );
+}
+function Test() {
+    return (
+        <>
+            <img src="src/photo.avif" alt="" />
+            Test
             <img src={photoPath} alt="" />
         </>
     );
 }
 
-export default App;
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Test />} />
+        </Routes>
+    );
+}
 
